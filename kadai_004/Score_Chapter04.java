051f58ae027eef2928aa04513858e7f859d730c1
Iwa1222 <131536129+Iwa1222@users.noreply.github.com>
2023-08-19 22:47:09 +0900

    Add files via upload

---
+++ kadai_004/Score_Chapter04.java
@@ -0,0 +1,21 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		String[] name = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+		int[] score = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(name[i] + "さんの点数は" + score[i] + "点");
+			
+		}
+		int total = 0;
+		for (int i = 0; i < score.length; i++) {
+			total += score[i];
+		}
+		int avg = total / score.length;
+		System.out.println("テストの平均点は" + avg + "点");
+	}
+
+}
